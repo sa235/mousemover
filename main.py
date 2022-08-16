77c423a1c52be36c77fdb2e7915f340bc52f5b63
Sam <sa235@live.ru>
2022-08-16 17:25:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import ctypes
 import time
 import math
-import keyboard
+
+
+def send_input_f15():
+    vk_f15 = (0x7E) # F15
+    ctypes.windll.user32.SendInput(1, vk_f15, 1)
 
 
 def do_move():
     i = 0
     while i < 28800:
-        y = 666 + int(235 * math.sin(i * math.pi/180))
-        x = 777 + int(235 * math.cos(i * math.pi/180))
+        y = 666 + int(235 * math.sin(i * math.pi / 180))
+        x = 777 + int(235 * math.cos(i * math.pi / 180))
         print(i, x, y)
         ctypes.windll.user32.SetCursorPos(x, y)
-        keyboard.SendInput(keyboard.Keyboard(keyboard.VK_F15))
+        send_input_f15()
         time.sleep(60)
         i = i + 1
 
